113cd316b6be8d9ab55bb56e6a6543efc4a71727
Timothy Norris <tibben@ocf.berkeley.edu>
2024-01-23 21:18:25 -0500

    functioning GDSC build

--- docker/README.md
+++ docker/README.md
@@ -13,16 +13,16 @@ See the docker README for documentation on how to use.
 
 Build the docker container for the front end and push to dockerhub. Build takes these options:  
 
-`       -h          to see this help message
-       -r          to build repository search interface
-       -p          push to dockerhub (defaults to yes)
-       -u <user>   specify the user account on dockerhub (optional, defaults to tibben)
+`       -h          to see this help message  
+       -r          to build repository search interface  
+       -p          push to dockerhub (defaults to yes)  
+       -u <user>   specify the user account on dockerhub (optional, defaults to tibben)  
        -n <name>   specify the repository name on dockerhub (optional, defaults to pg)`
 
 Runs a variant of these commands:
 
-`cd ../
-docker build -t $user/$name:$tag -f ./docker/$tag/Dockerfile .
+`cd ../  
+docker build -t $user/$name:$tag -f ./docker/$tag/Dockerfile .  
 docker push $user/$name:$tag`
 
 Note, you must be logged into docker hub as `<user>`.
--- docker/build.sh
+++ docker/build.sh
@@ -9,17 +9,18 @@
 tag=0
 user='tibben'
 name='pg'
-push=1
-while getopts ":h:r:u:n:p" option; do
+push=0
+while getopts ":hrpu:n:" option; do
+  echo ${option} - ${OPTARG}
   case $option in
     h) # help
-       echo 'Usage: $ ./build -hrsRp -u <user> -n <name>'
+       echo 'Usage: $ ./build -hrp -u <user> -n <name>'
        echo 'Options: '
        echo '-h          to see this help message'
        echo '-r          to build repository search interface container '
-       echo '-p          to push image to dockerhub (optional, defaults to yes)'
-       echo '-u <user>   specify the user account on dockerhub (optional, default=tibben)'
-       echo '-n <name>   specify the repository name on dockerhub (optional, default=pg)'
+       echo '-p          to push image to dockerhub (optional, default push=0)'
+       echo '-u <user>   specify the user account on dockerhub (optional, default user=tibben)'
+       echo '-n <name>   specify the repository name on dockerhub (optional, default name=pg)'
        echo 'NOTE: only one image can be built at a time, build options are exclusive.'
        exit;;
     r) # build the gdsc interface image
@@ -57,7 +58,10 @@ fi
 cd ../
 docker build -t $user/$name:$tag -f ./docker/$tag/Dockerfile .
 # must be logged into docker hub as $user ($ docker login)
+
+echo $push
 if [[ ${push} == 1 ]]
 then
+  echo docker push $user/$name:$tag
   docker push $user/$name:$tag
 fi
