e8943c37750b9436c4bfbccbd5838e999022e0e1
Timothy Norris <tibben@ocf.berkeley.edu>
2024-10-08 22:40:23 -0400

    solr with indices

--- builds/solr/ohdsi-init-solr.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./bin/solr create -c gaia
-echo '<delete><query>*:*</query></delete>' | ./bin/post -c gaia -d
-./bin/post -c gaia -filetypes json $(find /collections -name 'dcat_*.json' -type f)
