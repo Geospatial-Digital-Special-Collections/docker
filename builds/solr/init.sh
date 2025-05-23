939386b90b30e7c6a851b9671d70710c4fdbd139
Timothy Norris <tibben@ocf.berkeley.edu>
2025-05-23 09:24:15 -0400

    debug:solr index on init

--- builds/solr/init.sh
+++ builds/solr/init.sh
@@ -10,9 +10,12 @@ SOLR_OPTS="-Djetty.host=0.0.0.0"
 
 # Start Solr in the foreground
 echo "Starting Solr with custom configuration..."
-./bin/solr start -f $SOLR_OPTS
+./bin/solr start $SOLR_OPTS
 
 # build the intial indexes for collections and data respectively
 echo "indexing the collections"
 ./bin/solr post --solr-url http://localhost:8983 -c collections -filetypes json $(find /data/collections -name 'meta_*.json' -type f)
 ./bin/solr post --solr-url http://localhost:8983 -c dcat -filetypes json $(find /data/data -name 'meta_dcat*.json' -type f)
+
+# leave the container waiting
+tail -f /dev/null
