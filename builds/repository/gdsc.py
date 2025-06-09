76102ee187d0d809f7961507f23c8ed2ba999391
Ivan Kolesov <kolesov.id@gmail.com>
2025-06-09 13:39:57 -0700

    added all files that constitute the bibliography view feature

--- builds/repository/gdsc.py
+++ builds/repository/gdsc.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, url_for
 from urllib.request import urlopen
 from urllib.parse import urlencode
 import simplejson
@@ -14,38 +14,31 @@ BASE_PATH = 'http://solr.gdsc:8983/solr/dcat/select?wt=json&'
 SNIP_LENGTH = 180
 QUERY_FIELDS = ['gdsc_collections','dct_title','dcat_keyword','dct_description','gdsc_attributes']
 
-##
- # get solr data
- ##
-def query_solr(path,parameters):
-
+def query_solr(path, parameters):
     numresults = 1
     results = []
 
-    # send query to SOLR and gather paged results
-    query_string  = urlencode(parameters).replace('-','+')
+    query_string = urlencode(parameters).replace('-', '+')
     while numresults > len(results):
-        connection = urlopen("{}{}".format(path, query_string))
+        connection = urlopen(f"{path}{query_string}")
         response = simplejson.load(connection)
         numresults = response['response']['numFound']
         results = response['response']['docs']
         parameters["rows"] = numresults
-        query_string  = urlencode(parameters).replace('-','+')
-    if results == None: results = []
+        query_string = urlencode(parameters).replace('-', '+')
+    if results is None:
+        results = []
 
     return results, numresults
 
-##
- # highlight found instances of query in document metadata
- ##
-def highlight_query(document,query):
-
-    def add_tags(string_value,query):
+def highlight_query(document, query):
+    def add_tags(string_value, term):
         return re.sub(
-            r'(' + term  + ')',
-            '<span class="highlight-term">\g<1></span>',
+            rf'({term})',
+            '<span class="highlight-term">\\1</span>',
             string_value,
-            flags=re.IGNORECASE)
+            flags=re.IGNORECASE
+        )
 
     document['found_in'] = {}
     for field in QUERY_FIELDS:
@@ -53,29 +46,23 @@ def highlight_query(document,query):
             attrs = []
             for i, attr in enumerate(document[field]):
                 terms = query.split(' ')
-                found = True
-                for term in terms:
-                    if term.upper() not in attr.upper(): found = False
+                found = all(term.upper() in attr.upper() for term in terms)
                 if found:
                     document['found_in'][field] = []
                     for term in terms:
-                        document[field][i] = add_tags(document[field][i],term)
+                        document[field][i] = add_tags(document[field][i], term)
                     row = attr.split(';')
                     if len(row) > 1:
-                        document[field][i] = add_tags(document[field][i],row[0])
-                        for j in range(0,2):
+                        for j in range(2):
                             for term in terms:
-                                row[j] = add_tags(row[j],term)
-                        row[0] = add_tags(row[0],row[0])
-                        attrs.append([row[0],row[1]])
-            if len(attrs) > 0: document['found_in'][field] = attrs
+                                row[j] = add_tags(row[j], term)
+                        attrs.append([row[0], row[1]])
+            if attrs:
+                document['found_in'][field] = attrs
 
     return document
 
-##
- # run SOLR query and render results for main page
- ##
-@app.route('/', methods=["GET","POST"])
+@app.route('/', methods=["GET", "POST"])
 def index():
     collection = 'all'
     query, active = None, None
@@ -84,60 +71,50 @@ def index():
     numresults = 1
     results = []
 
-    # get form parameters and build SOLR query parameters
     if request.method == "POST":
-
-        # get the form parameters
-        if 'ImmutableMultiDict' in str(type(request.form)): args = request.form.to_dict()
-        else: args = request.form
+        if 'ImmutableMultiDict' in str(type(request.form)):
+            args = request.form.to_dict()
+        else:
+            args = request.form
         if 'searchTerm' in args:
-            query = re.sub(r'[\+\-\&\|\!\(\)\{\}\[\]\^\"\~\*\?\:\\]','',args["searchTerm"])
-        if query == "None" or query == "": query = None
-        collection = args["collection"]
+            query = re.sub(r'[\+\-\&\|\!\(\)\{\}\[\]\^\"\~\*\?\\:\\]', '', args["searchTerm"])
+        if query in ("None", ""):
+            query = None
+        collection = args.get("collection", collection)
         if 'active' in args:
-            active = args["active"]
-            if active == 'None': active = None
+            active = args["active"] or None
 
-        # build the query parameters for SOLR
         q = collection
-        if collection == 'all' or collection == '*':
+        if collection in ('all', '*'):
             collection = '*'
             q = ""
-        query_parameters = {"q": "gdsc_collections:" + collection}
-        if query is not None:
+        query_parameters = {"q": f"gdsc_collections:{collection}"}
+        if query:
             qf += ' '.join(QUERY_FIELDS)
-            if len(q) > 0: q += " "
-            q += query
-        if active is not None:
-            if qf != "gdsc_collections ": qf += " "
-            qf += "gdsc_up"
-            if len(q) > 0: q += " "
-            q += "true"
-        if qf != "gdsc_collections ":
+            q = f"{q} {query}".strip()
+        if active:
+            qf = f"{qf} gdsc_up"
+            q = f"{q} true".strip()
+        if qf.strip() != "gdsc_collections":
             query_parameters = {
-              "q.op": "AND",
-              "defType": "dismax",
-              "qf": qf,
-              "q": q
+                "q.op": "AND",
+                "defType": "dismax",
+                "qf": qf,
+                "q": q
             }
 
-    # send query to SOLR and gather paged results
-    results, numresults = query_solr(BASE_PATH,query_parameters)
+    results, numresults = query_solr(BASE_PATH, query_parameters)
 
-    # check results for correct display
     for entry in results:
-
-        # highlight search term in results
-        if query != None and query != 'None' and query != '':
-            entry = highlight_query(entry,query)
-
-        # snip abstracts
-        if entry['dct_description']:
+        if query:
+            entry = highlight_query(entry, query)
+        if entry.get('dct_description'):
             entry['display_description'] = entry['dct_description'][0]
             if len(entry['display_description']) > SNIP_LENGTH:
-                entry['display_description'] = entry['dct_description'][0][0:SNIP_LENGTH] + '...'
+                entry['display_description'] = entry['dct_description'][0][:SNIP_LENGTH] + '...'
 
-    if collection == "*": collection = 'all'
+    if collection == "*":
+        collection = 'all'
 
     return render_template(
         'index.html',
@@ -146,72 +123,134 @@ def index():
         active=active,
         numresults=numresults,
         results=results,
-        collections=COLLECTIONS
+        collections=COLLECTIONS,
+        switch_url=url_for('collections_view'),
+        switch_label='Collections'
     )
 
-##
- # query SOLR for one document and render all metadata in detail
- ##
-@app.route('/detail/<name_id>', methods=["GET","POST"])
+@app.route('/detail/<name_id>', methods=["GET", "POST"])
 def detail(name_id):
-
     args = request.args.to_dict()
 
-    query_parameters = {"q": "gdsc_tablename:" + name_id}
-    query_string  = urlencode(query_parameters)
-    connection = urlopen("{}{}".format(BASE_PATH, query_string))
+    query_parameters = {"q": f"gdsc_tablename:{name_id}"}
+    query_string = urlencode(query_parameters)
+    connection = urlopen(f"{BASE_PATH}{query_string}")
     response = simplejson.load(connection)
     document = response['response']['docs'][0]
 
     if 'gdsc_attributes' in document:
         document['gdsc_columns'] = [attr.split(';')[0] for attr in document['gdsc_attributes']]
 
-    if args['query'] != None and args['query'] != 'None' and args['query'] != '':
-        document = highlight_query(document,args['query'])
+    if args.get('query'):
+        document = highlight_query(document, args['query'])
 
     if 'gdsc_attributes' in document:
         document['gdsc_attributes'] = [attr.split(';') for attr in document['gdsc_attributes']]
 
     if 'gdsc_derivatives' in document:
-        document['gdsc_derived'] = [attr.split(';') for attr in document['gdsc_derived']]
+        document['gdsc_derived'] = [attr.split(';') for attr in document['gdsc_derivatives']]
 
     return render_template('detail.html', name_id=name_id, document=document, referrer=request.args)
 
-##
- # provide download api for derivate files
- ##
-@app.route('/download/<path:download_path>', methods=["GET","POST"])
+@app.route('/download/<path:download_path>', methods=["GET", "POST"])
 def download(download_path):
+    if 'ImmutableMultiDict' in str(type(request.args)):
+        args = request.args.to_dict()
+    else:
+        args = request.args
 
-    if 'ImmutableMultiDict' in str(type(request.args)): args = request.args.to_dict()
-    else: args = request.args
-
-    if 'format' in args:
-        if args['format'] in ["sql","shp","geotiff"]:
-            return send_from_directory(
-                f"/{download_path}derived/",
-                f"{args['file']}.{args['format']}.tar.gz",
-                as_attachment=True
-            )
+    if 'format' in args and args['format'] in ("sql", "shp", "geotiff"):
+        return send_from_directory(
+            f"/{download_path}derived/",
+            f"{args['file']}.{args['format']}.tar.gz",
+            as_attachment=True
+        )
 
     return "File not found", 400
 
-##
- # always get the list of collections for reference
- ##
+# Get list of collections
 COLLECTIONS, COLLECTIONS_COUNT = query_solr(
     'http://solr.gdsc:8983/solr/collections/select?wt=json&',
     {
-      "q.op": "OR",
-      "q": "Status:published"
+        "q.op": "OR",
+        "q": "Status:published"
     }
 )
+
+# 🔧 Ensure all items have a 'Creator' field
+for item in COLLECTIONS:
+    if 'Creator' not in item:
+        item['Creator'] = [""]
+
 keys = [item['Collection_ID'][0] for item in COLLECTIONS]
 COLLECTIONS = dict(zip(keys, COLLECTIONS))
 COLLECTIONS = OrderedDict(sorted(COLLECTIONS.items(), key=lambda i: i[0].lower()))
 
-##
- # run the app if called from the command line
- ##
+@app.route('/collections', methods=["GET", "POST"])
+def collections_view():
+    collection = list(COLLECTIONS.keys())[0]
+    query, active = None, None
+    query_parameters = {"q": "gdsc_collections:*"}
+    q, qf = "", "gdsc_collections "
+    numresults = 1
+    results = []
+
+    if request.method == "POST":
+        if 'ImmutableMultiDict' in str(type(request.form)):
+            args = request.form.to_dict()
+        else:
+            args = request.form
+        if 'searchTerm' in args:
+            query = re.sub(r'[\+\-\&\|\!\(\)\{\}\[\]\^\"\~\*\?\\:\\]', '', args["searchTerm"])
+        if query in ("None", ""):
+            query = None
+        collection = args.get("collection", collection)
+        if 'active' in args:
+            active = args['active'] or None
+
+        q = collection
+        if collection in ('all', '*'):
+            collection = '*'
+            q = ""
+        query_parameters = {"q": f"gdsc_collections:{collection}"}
+        if query:
+            qf += ' '.join(QUERY_FIELDS)
+            q = f"{q} {query}".strip()
+        if active:
+            qf = f"{qf} gdsc_up"
+            q = f"{q} true".strip()
+        if qf.strip() != "gdsc_collections":
+            query_parameters = {
+                "q.op": "AND",
+                "defType": "dismax",
+                "qf": qf,
+                "q": q
+            }
+
+    results, numresults = query_solr(BASE_PATH, query_parameters)
+
+    for entry in results:
+        if query:
+            entry = highlight_query(entry, query)
+        if entry.get('dct_description'):
+            entry['display_description'] = entry['dct_description'][0]
+            if len(entry['display_description']) > SNIP_LENGTH:
+                entry['display_description'] = entry['dct_description'][0][:SNIP_LENGTH] + '...'
+
+    if collection == "*":
+        collection = 'all'
+
+    return render_template(
+        'collections.html',
+        collection=collection,
+        query=query,
+        active=active,
+        numresults=numresults,
+        results=results,
+        collections=COLLECTIONS,
+        switch_url=url_for('index'),
+        switch_label='Home'
+    )
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True,use_reloader=True)
+    app.run(host='0.0.0.0', debug=True, use_reloader=True)
